e184059a8322b05625d2ac75b43400dd7526bc0a
Rodrigo(Zara) <rodzmaciel21@gmail.com>
2026-04-20 11:40:26 -0300

    cria modelo customizado de usuario com roles

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('aluno', 'Aluno'),
+        ('professor', 'Professor'),
+        ('responsavel', 'Responsável'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    matricula = models.CharField(max_length=20, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
